00ac60244ec1f7e8d0b9b06a4caabf6cce16ea0d
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-12-27 17:55:40 +0100

    TASK: refactor jsonQueryHelper from http matching

---
+++ behavioral-tests/jsonQueryHelper/jsonQueryHelper.go
@@ -0,0 +1,87 @@
+package jsonQueryHelper
+
+import (
+	"fmt"
+	"strings"
+	"reflect"
+	"time"
+	"regexp"
+	"github.com/elgs/gojq"
+	"github.com/DATA-DOG/godog/gherkin"
+	"math"
+	"strconv"
+)
+
+func AssertJsonStructure(jsonString string, comparators *gherkin.DataTable) error {
+	jsonQuery, err := gojq.NewStringQuery(jsonString)
+
+	if err != nil {
+		return fmt.Errorf("Cannot deserialize JSON response: %s", string(jsonString))
+	}
+
+	for i, value := range comparators.Rows {
+		if len(value.Cells) != 3 {
+			return fmt.Errorf("every comparison needs to be written in exactly 3 columns; but in row %d I found %d.", i+1, len(value.Cells))
+		}
+
+		fieldPath := value.Cells[0].Value
+		comparator := value.Cells[1].Value
+		operand := value.Cells[2].Value
+
+		value, err := jsonQuery.Query(fieldPath)
+
+		if comparator != "is empty" && err != nil {
+			return fmt.Errorf("Error doing query %v: %v. JSON: %v", fieldPath, err, jsonString)
+		}
+
+		switch comparator {
+		case "equals":
+			switch value.(type) {
+			case string:
+				if strings.Compare(value.(string), operand) != 0 {
+					return fmt.Errorf("String '%s' is not equal to expected string '%s' at path %s. (line %d)", value, operand, fieldPath, i)
+				}
+			case float64:
+				floatOperand, _ := strconv.ParseFloat(operand, 64)
+				if math.Abs(value.(float64) - floatOperand) > 0.0000001 {
+					return fmt.Errorf("Number '%d' is not equal to expected number '%d' at path %s. (line %d)", value, operand, fieldPath, i)
+				}
+			default:
+				return fmt.Errorf("equals comparison for unknown type value in line %d - type was: %s", i, reflect.TypeOf(value))
+			}
+		case "is a date":
+			switch value.(type) {
+			case string:
+				_, e := time.Parse(time.RFC3339, value.(string))
+				if e != nil {
+					return fmt.Errorf("Timestamp '%s' could not be parsed at path %s. (line %d)", value, fieldPath, i)
+				}
+			default:
+				return fmt.Errorf("'is a date' comparison for unknown type value in line %d - type was: %s", i, reflect.TypeOf(value))
+			}
+
+		case "matches regex":
+			switch value.(type) {
+			case string:
+				hasMatched, err := regexp.MatchString(operand, value.(string))
+				if err != nil {
+					return err
+				}
+				if !hasMatched {
+					return fmt.Errorf("String '%s' does not match to expected regex '%s' at path %s. (line %d)", value, operand, fieldPath, i)
+				}
+			default:
+				return fmt.Errorf("matches regex' comparison in line %d only works for type string - type was: %s", i, reflect.TypeOf(value))
+			}
+		case "is empty":
+			if value != nil && len(value.([]interface{})) > 0 {
+				return fmt.Errorf("'%v' is not empty at path %s. (line %d)", value, fieldPath, i)
+			}
+		default:
+			return fmt.Errorf("Comparator %s not supported (line %d)", comparator, i)
+		}
+
+	}
+
+	return nil
+}
--- behavioral-tests/steps_mockHttpServer_test.go
+++ behavioral-tests/steps_mockHttpServer_test.go
@@ -3,17 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/DATA-DOG/godog/gherkin"
-	"github.com/elgs/gojq"
 	"github.com/sandstorm/dokku-enterprise-plugin/behavioral-tests/httpServerStoppableListener"
 	"io/ioutil"
 	"net"
 	"net/http"
-	"reflect"
-	"regexp"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
+	"github.com/sandstorm/dokku-enterprise-plugin/behavioral-tests/jsonQueryHelper"
 )
 
 // this waitGroup is used to block until the application server has fully shut down.
@@ -142,65 +139,5 @@ func theAPIDeliveryHttpServerReceivedTheFollowingJSONAtEvent(requestNumber int, 
 		return fmt.Errorf("Expected request URL '%s' does not match actual '%s'", url, request.url)
 	}
 
-	jsonQuery, err := gojq.NewStringQuery(string(request.body[:]))
-
-	if err != nil {
-		return fmt.Errorf("Cannot deserialize JSON response: %s", string(request.body[:]))
-	}
-
-	for i, value := range comparators.Rows {
-		if len(value.Cells) != 3 {
-			return fmt.Errorf("every comparison needs to be written in exactly 3 columns; but in row %d I found %d.", i+1, len(value.Cells))
-		}
-
-		fieldPath := value.Cells[0].Value
-		comparator := value.Cells[1].Value
-		operand := value.Cells[2].Value
-
-		value, err := jsonQuery.Query(fieldPath)
-		if err != nil {
-			return err
-		}
-
-		switch comparator {
-		case "equals":
-			switch value.(type) {
-			case string:
-				if strings.Compare(value.(string), operand) != 0 {
-					return fmt.Errorf("String '%s' is not equal to expected string '%s' at path %s. (line %d)", value, operand, fieldPath, i)
-				}
-			default:
-				return fmt.Errorf("equals comparison for unknown type value in line %d - type was: %s", i, reflect.TypeOf(value))
-			}
-		case "is a date":
-			switch value.(type) {
-			case string:
-				_, e := time.Parse(time.RFC3339, value.(string))
-				if e != nil {
-					return fmt.Errorf("Timestamp '%s' could not be parsed at path %s. (line %d)", value, fieldPath, i)
-				}
-			default:
-				return fmt.Errorf("'is a date' comparison for unknown type value in line %d - type was: %s", i, reflect.TypeOf(value))
-			}
-
-		case "matches regex":
-			switch value.(type) {
-			case string:
-				hasMatched, err := regexp.MatchString(operand, value.(string))
-				if err != nil {
-					return err
-				}
-				if !hasMatched {
-					return fmt.Errorf("String '%s' does not match to expected regex '%s' at path %s. (line %d)", value, operand, fieldPath, i)
-				}
-			default:
-				return fmt.Errorf("matches regex' comparison in line %d only works for type string - type was: %s", i, reflect.TypeOf(value))
-			}
-		default:
-			return fmt.Errorf("Comparator %s not supported (line %d)", comparator, i)
-		}
-
-	}
-
-	return nil
+	return jsonQueryHelper.AssertJsonStructure(string(request.body[:]), comparators)
 }
